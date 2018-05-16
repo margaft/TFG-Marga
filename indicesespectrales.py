30d38f06a2ee78b8c3d50d5e89796710ac062fac
obarquero <obarquero@gmail.com>
2018-05-16 08:20:55 +0200

    Solving interpolation
    New rr example
    Solving interpolation

--- indicesespectrales.py
+++ indicesespectrales.py
@@ -7,13 +7,14 @@ Created on Sun Apr  8 10:35:33 2018
 
 import numpy as np
 from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
 
 #Función que calcula los índices espectrales de una serie temporal de intervalo rr
 #Duración 5 minutos o más.
 #t tiempos de vector de raw rr, es decir, el tiempo original en mseg. Si no
 #pasado, se calculará a partir de series temporales de intervalos rr.
 
-def interp_to_psd(rr, t = None, fs = 4., method = 'spline'):
+def interp_to_psd(rr, t = None, fs = 4., method = 'cubic'):
     
     #En python el control de parámetros es diferente
     if t == None:
@@ -22,21 +23,32 @@ def interp_to_psd(rr, t = None, fs = 4., method = 'spline'):
 
     ts = 1/fs #sampling frequency
     
-    t_new = np.arange(t[0],t[-1],ts) #nuevo vector para la interopolacion
+    t_new = np.arange(t[0],t[-1],ts) #nuevo vector para la interpolacion
     #Interpolacion
-    rr_interp = interp1d(t, rr, t_new, method)
-
+    f = interp1d(t, rr, kind = method) #crea el objeto para interpolar
+    
+    #ahora realizamos la interpolación realmente
+    rr_interp = f(t_new)
+    
+    return rr_interp,t_new
 
 def main_spectral(rr,t = None,duration=5):
     if t == None:
-        t = np.cumsum(rr)/1000;
+        t = np.cumsum(rr)/1000.
 
-#Interpolación
+    #Interpolación
     
-    rr_interpolated_4_hz = interp_to_psd(rr,t, 0, 0);
+    rr_interpolated_4_hz,t_new = interp_to_psd(rr,t)
     
+    return rr_interpolated_4_hz, t_new
 
-main_spectral(rrtxt)
+
+rr = np.load('rr_example.npy')
+t = np.cumsum(rr)/1000.
+plt.plot(t,rr)
+rr_interpolated, t_new = main_spectral(rr)
+
+plt.plot(t,rr_interpolated)
 
 
 
